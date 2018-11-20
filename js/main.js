e85142bfcc8374c6376d255e2a983e18d889bd61
ivanklymiv <ivanklimiv@gmail.com>
2018-11-20 17:54:58 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.ba-slider').slick({
+   "slidesToShow": 4, "slidesToScroll": 1,
+   infinite: true,
+   nextArrow: '.ba-next',
+   prevArrow: '.ba-prev',
+   arrows: true,
+
+  });
+});
